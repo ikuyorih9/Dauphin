6a2d926c04ba3e547e9a7604885c20dc6807120f
Hugo Hiroyuki Nakamura <hugo.nakamura@opus-software.com.br>
2025-01-22 12:05:42 -0300

    SpringBoot project template

---
+++ dauphin/src/main/java/com/dauphin/dauphin/DauphinApplication.java
@@ -0,0 +1,13 @@
+package com.dauphin.dauphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DauphinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DauphinApplication.class, args);
+	}
+
+}
---
+++ dauphin/src/test/java/com/dauphin/dauphin/DauphinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphin.dauphin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DauphinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
