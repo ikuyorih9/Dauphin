aa8d127705fe8494a4e20603b28c53357d2554a7
ikuyorih9 <hugohiroyuki9@gmail.com>
2025-01-27 20:22:26 -0300

    [API] GET e POST implementados

---
+++ dauphin/src/main/java/com/dauphin/dauphin/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.dauphin.dauphin.repository;
+
+import com.dauphin.dauphin.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+}
