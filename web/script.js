15f96def5e471d94f596d2102e49fa48a9bf4036
ikuyorih9 <hugohiroyuki9@gmail.com>
2025-01-28 13:48:27 -0300

    [WEB] script.js com POST para o usuário

--- web/script.js
+++ web/script.js
@@ -3,9 +3,35 @@ LoginForm = document.getElementById('login_form')
 LoginForm.addEventListener('submit', function(event){
     event.preventDefault();
 
-    const user = document.getElementById('login_input').value;
+    const username = document.getElementById('login_input').value;
     const senha = document.getElementById('senha_input').value;
 
-    console.log(user);
-    console.log(senha);
+    const usuario = {
+        username:username,
+        senha:senha
+    }
+
+    fetch('http://localhost:8080/api/auth/login' , {
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    })
+    .then(response=>{
+        if(response.ok){
+            return response.text();
+        }
+        else{
+            throw new Error('Usuário ou senha inválidos.')
+        }
+    })
+    .then(data=>{
+        console.log(data);
+        alert('Login bem-sucedido');
+    })
+    .catch(error=>{
+        console.error(error);
+        alert(error.message);
+    });
 });
